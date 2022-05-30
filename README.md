cb3ff9d1b85499f994b95a225c0729973e1ec38c
AndreyPootMay <andreypootmay@gmail.com>
2022-05-30 13:38:48 -0500

    :heavy_check_mark: First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Redis Node Caching
+
+This repository show how to use node js with redis for caching rest api responses for do it more quckly.
+
+## Install
+
+1. Clone the repo
+2. Navigate into the repo and use `npm install`.
+3. If you don't have any redis instance installed before, try to use a docker image `docker run --name my-redis -p 6379:6379 -d redis`.
+
+Credits to faztweb for that tutorial, appreciated.
+
+2022 - Niger Andrey Poot May
---
+++ src/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const axios = require('axios');
+const { createClient } = require('redis');
+const responseTime = require('response-time');
+
+const client = createClient({
+    host: "127.0.0.1",
+    port: 6379
+});
+
+client.on("error", function(err){
+    console.error("Error encountered: ", err);
+});
+
+client.on("connect", () => {
+    console.log("Redis connected");
+});
+
+const app = express();
+
+app.use(responseTime());
+
+app.get("/character", async (req, res) => {
+    try {
+        const reply = await client.get("characters");
+
+        if (reply) {
+            return res.json(JSON.parse(reply));
+        }
+
+        const response = await axios.get("https://rickandmortyapi.com/api/character");
+
+        await client.set('characters', JSON.stringify(response.data));
+
+        res.json(response.data);
+    } catch (error) {
+        return res.status(error.response.status).json({ message: error.message });
+    }
+});
+
+app.get('/character/:id', async (req, res) => {
+    try {
+        const reply = await client.get(req.originalUrl);
+
+        if (reply) return res.json(JSON.parse(reply));
+
+        const response = await axios.get(
+            `https://rickandmortyapi.com/api/character/${req.params.id}`
+        );
+
+        await client.set(req.originalUrl, JSON.stringify(response.data));
+
+        return res.json(response.data);
+    } catch (error) {
+        return res.status(error.response.code).json({ message: error.message });
+    }
+});
+
+app.get('/hello', (req, res) => {
+    res.json({message: "hola", date: new Date().getFullYear()});
+});
+
+async function main() {
+    await client.connect();
+    app.listen(3000);
+
+    console.log('Server on port 3000');
+}
+
+main();
